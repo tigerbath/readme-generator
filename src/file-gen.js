ba0bddc38c4adc821e86cbbc73683e994f8c6c2a
Matthew Palmer <mattjpalmer92@gmail.com>
2021-11-23 21:10:56 +0000

    fixed bug in readFile function

--- src/file-gen.js
+++ src/file-gen.js
@@ -2,32 +2,32 @@ const fs = require("fs");
 
 // Read file
 const readFile = (filePath) => {
-    try {
-        const content = fs.readFileSync, "utf8";
-        return content;
-    }   catch (error){
-        console.log(error.message);
-    }
+  try {
+    const content = fs.readFileSync(filePath);
+    return content;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 // Write file
 const writeFile = (filePath, data) => {
-    try {
-        fs.writeFileSync(filePath, data);
-    } catch (error){
-        console.log(error.message);
-    }
+  try {
+    fs.writeFileSync(filePath, data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 // Append file
 const appendFile = (filePath, data) => {
-    try {
-        fs.appendFileSync(filePath, data);
-    } catch (error) {
-        console.log(error.message);
-    }
+  try {
+    fs.appendFileSync(filePath, data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 // Export
 module.exports = {
-    readFile,
-    writeFile,
-    appendFile,
+  readFile,
+  writeFile,
+  appendFile,
 };
