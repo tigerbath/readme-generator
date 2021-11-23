77c7d74e5307da7be447f607d29196445d39ff72
Matthew Palmer <mattjpalmer92@gmail.com>
2021-11-23 20:39:21 +0000

    formatted readfile const in file-gen.js

--- src/file-gen.js
+++ src/file-gen.js
@@ -1,7 +1,14 @@
 const fs = require("fs");
 
 // Read file
-const readFile = () => {};
+const readFile = (filePath) => {
+    try {
+        const content = fs.readFileSync, "utf8";
+        return content;
+    }   catch (error){
+        console.log(error.message);
+    }
+};
 // Write file
 const writeFile = () => {};
 // Append file
