c0db023a1d43d39cb5232107b902ead9119af011
Matthew Palmer <mattjpalmer92@gmail.com>
2021-11-23 18:07:27 +0000

    tidied up index.js

--- src/index.js
+++ src/index.js
@@ -7,112 +7,48 @@ const questions = [
   {
     type: "input",
     message: "What is the title of your project?",
-    name: "title"
-  },{
+    name: "title",
+  },
+  {
     type: "input",
     message: "Please give a description of your project",
-    name: "description"
-  },{
+    name: "description",
+  },
+  {
     type: "input",
     message: "Does your project require installation information?",
-    name: "installation"
-  },{
+    name: "installation",
+  },
+  {
     type: "input",
     message: "Has this project been tested?",
-    name: "test"
-  },{
+    name: "test",
+  },
+  {
     type: "input",
     message: "What are the usage rights?",
-    name: "usage"
-  },{
+    name: "usage",
+  },
+  {
     type: "list",
     message: "Who contributed to this project?",
-    name: "contributions"
-  },{
+    name: "contributions",
+  },
+  {
     type: "list",
     message: "Please select what license you used",
     name: "license",
-    choices : [
+    choices: [
       "APACHE 2.0",
       "BSD 3",
       "BSD 2",
       "MIT",
-      "MOZILLA PUBLIC LICENSE 2.0"
-    ]
-    }
-  }
+      "MOZILLA PUBLIC LICENSE 2.0",
+    ],
+  },
 ];
 
-//generate questions
-const generateTitle = (answers) => {
-  return;
-  `# title ![MIT](https://img.shields.io/static/v1?label=MIT&message=license&color=blue)`;
-};
-
-const generateTableOfContents = (answers) => {
-  return `## Table of contents
-
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Tests](#tests)
-    - [Contributing](#contributing)
-    - [License](#license)
-    `;
-};
-
-const generateDescription = (answers) => {
-    return `## Description
-
-    ADD DESCRIPTION HERE`
-};
-// use turnary operator
-const generateInstallation = (answers) => {
-    return `## Installation
-
-    Run the following script to install the packages required for this application:
-    
-    \`\`\`
-    ADD SCRIPT HERE
-    \`\`\`
-    `
-
-};
-//use turnary operator
-const generateUsage = (answers) => {
-    return `## Usage
-
-    To use the application run the following script:
-    
-    \`\`\`
-    ADD SCRIPT HERE
-    \`\`\`
-    `
-};
-
-const generateTests = (answers) => {
-    return `## Tests
-
-    To use the application run the following script:
-    
-    \`\`\`
-    ADD TEST HERE
-    \`\`\`
-    `
-};
-
-const generateContributing = (answers) => {
-    return `## Contributing
-
-    ADD TEXT HERE`
-};
-
-const generateLicense = (answers) => {
-    return `## License
-
-    ADD LICENSE HERE`
-};
-// Generate readme document 
+// Generate readme document
 const generateReadme = (answers) => {
   return `${generateTitle(answers)}
   
@@ -131,7 +67,6 @@ const generateReadme = (answers) => {
   ${utils.generateLicense(answers)}
   `;
 };
-
 
 // use async await to initialize user interaction
 const init = async () => {
