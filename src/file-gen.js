0744c4f8cc87f7005b5fde6a5e87bc686d5ae01f
Matthew Palmer <mattjpalmer92@gmail.com>
2021-11-23 17:39:27 +0000

    added functions to file-gen.js

---
+++ src/file-gen.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+// Read file
+const readFile = () => {};
+// Write file
+const writeFile = () => {};
+// Append file
+const appendFile = () => {};
+// Export
+const exportFile = () => {};
